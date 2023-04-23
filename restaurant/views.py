ccc84f136792b6cc18e4243709ed64763d0d1f5a
RRitaa <reeplex01@gmail.com>
2023-04-22 22:29:06 -0400

    Updated API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+
+from .serializers import MenuSerializer
+from .models import Menu
 
 # Create your views here.
+
+class MenuItemsView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):  
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    
+class BookingViewSet(viewsets.ModelViewSet): 
+    permission_classes = [IsAuthenticated] 
+    queryset = Menu.objects.all()
+
+
+
+
+
 def index(request):
     return render(request, 'index.html', {})
+
+
