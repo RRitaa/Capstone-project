72089c6aef96261de24ce9cff7c22fd15b011468
RRitaa <reeplex01@gmail.com>
2023-04-28 23:06:16 -0400

    updated database

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': BASE_DIR / 'db.sqlite3', 
+    },
+    
+    'littlelemon': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
